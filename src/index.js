59390c0d40bf42460b60c2daed313c94fadf1105
James Fenn <me@jfenn.me>
2020-01-25 14:49:29 -0500

    update index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { compose, wrapCompose, render, renderElement } = require('./compose.js');
 const { observe } = require('./util/resolvable.js');
 const elements = require('./elements.js');
+const util = require('./utilities.js');
 
-module.exports = { compose, wrapCompose, render, renderElement, elements, observe };
+module.exports = { compose, wrapCompose, render, renderElement, elements, observe, util };
